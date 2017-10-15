84f98847958a2fa67e1ba39760683e1c66de5948
leeson <filhype.bernard@gmail.com>
2017-10-15 17:15:10 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String searchString = "Hello World";
+            Console.WriteLine(searchString);
+
+            IWebDriver driver = new ChromeDriver();
+
+            //Launch the ToolsQA Website
+            driver.Url = "http://www.google.fr";
+
+            // Storing Title name in String variable
+            String Title = driver.Title;
+
+            // Storing Title length in Int variable
+            int TitleLength = driver.Title.Length;
+
+            // Printing Title name on Console
+            Console.WriteLine("Title of the page is : " + Title);
+
+            // Printing Title length on console
+            Console.WriteLine("Length of the Title is : " + TitleLength);
+
+            // Storing URL in String variable
+            String PageURL = driver.Url;
+
+            // Storing URL length in Int variable
+            int URLLength = PageURL.Length;
+
+            // Printing URL on Console
+            Console.WriteLine("URL of the page is : " + PageURL);
+
+            // Printing URL length on console
+            Console.WriteLine("Length of the URL is : " + URLLength);
+
+            // Storing Page Source in String variable
+            String PageSource = driver.PageSource;
+
+            // Storing Page Source length in Int variable
+            int PageSourceLength = driver.PageSource.Length;
+
+            // Printing Page Source on console
+            //Console.WriteLine("Page Source of the page is : " + PageSource);
+
+            // Printing Page SOurce length on console
+            Console.WriteLine("Length of the Page Source is : " + PageSourceLength);
+
+            // Find the text input element by its name
+            IWebElement query = driver.FindElement(By.Name("q"));
+
+            // Enter something to search for
+            query.SendKeys(searchString);
+
+            // Now submit the form. WebDriver will find the form for us from the element
+            query.Submit();
+
+            // Google's search is rendered dynamically with JavaScript.
+            // Wait for the page to load, timeout after 10 seconds
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.StartsWith(searchString, StringComparison.OrdinalIgnoreCase));
+
+            Console.ReadKey();
+
+            //Closing browser
+            driver.Quit();
+        }
+    }
+}
